7ca28e4cfdbf1581451c9e4c17fbe049a609f7f8
Лада Максимовна Муравьёва <s0135238@edu.kubsu.ru>
2020-11-08 18:15:18 +0300

    Working menu items. Raw. Two pages.

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -19,9 +19,24 @@ namespace WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        Pages.Home home = new Pages.Home();
+        Pages.Quotation1 quotation1 = new Pages.Quotation1();
+
+        //create change for buttons
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void page_home_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = home;
+        }
+
+        private void page_qu_1_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = quotation1;
         }
     }
 }
