35af2880457b542aeda0ad4884351f467abbd2cd
Грант Кесиян <grant.kesiyan@mail.ru>
2020-11-10 20:29:20 +0300

    Добавлена заглушка для класса взаимодействия с MS Word

---
+++ WPF/Utils/WorkWord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace WPF.Utils
+{
+    public class WorkWord
+    {
+        private Word.Application _application = null;
+
+        //TODO
+    }
+}
