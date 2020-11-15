79d0ed832f316f4c29a40181f0b560546bd449ce
NorovichukIrinaA <rinaalexn@mail.ru>
2020-11-15 20:11:06 +0300

    Home page Title. Polised menu. Logo. Styles for menu elements. Working Exit. Extra pages.

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -21,6 +21,12 @@ namespace WPF
     {
         Pages.Home home = new Pages.Home();
         Pages.Quotation1 quotation1 = new Pages.Quotation1();
+        Pages.Quotation2 quotation2 = new Pages.Quotation2();
+        Pages.Quotation3 quotation3 = new Pages.Quotation3();
+        Pages.Quotation4 quotation4 = new Pages.Quotation4();
+        Pages.Quotation5 quotation5 = new Pages.Quotation5();
+        Color focus_color = (Color)ColorConverter.ConvertFromString("#5a6baf");
+        Color reg_color = (Color)ColorConverter.ConvertFromString("#3d4b89");
 
         //create change for buttons
 
@@ -32,11 +38,77 @@ namespace WPF
         private void page_home_btn_Click(object sender, RoutedEventArgs e)
         {
             MainFrame.Content = home;
+            home_text.Foreground = new SolidColorBrush(focus_color);
+            page_1_text.Foreground = new SolidColorBrush(reg_color);
+            page_2_text.Foreground = new SolidColorBrush(reg_color);
+            page_3_text.Foreground = new SolidColorBrush(reg_color);
+            page_4_text.Foreground = new SolidColorBrush(reg_color);
+            page_5_text.Foreground = new SolidColorBrush(reg_color);
         }
 
         private void page_qu_1_btn_Click(object sender, RoutedEventArgs e)
         {
             MainFrame.Content = quotation1;
+            home_text.Foreground = new SolidColorBrush(reg_color);
+            page_1_text.Foreground = new SolidColorBrush(focus_color);
+            page_2_text.Foreground = new SolidColorBrush(reg_color);
+            page_3_text.Foreground = new SolidColorBrush(reg_color);
+            page_4_text.Foreground = new SolidColorBrush(reg_color);
+            page_5_text.Foreground = new SolidColorBrush(reg_color);
+
+        }
+
+        private void page_qu_2_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = quotation2;
+            home_text.Foreground = new SolidColorBrush(reg_color);
+            page_1_text.Foreground = new SolidColorBrush(reg_color);
+            page_2_text.Foreground = new SolidColorBrush(focus_color);
+            page_3_text.Foreground = new SolidColorBrush(reg_color);
+            page_4_text.Foreground = new SolidColorBrush(reg_color);
+            page_5_text.Foreground = new SolidColorBrush(reg_color);
+
+        }
+
+        private void page_qu_3_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = quotation3;
+            home_text.Foreground = new SolidColorBrush(reg_color);
+            page_1_text.Foreground = new SolidColorBrush(reg_color);
+            page_2_text.Foreground = new SolidColorBrush(reg_color);
+            page_3_text.Foreground = new SolidColorBrush(focus_color);
+            page_4_text.Foreground = new SolidColorBrush(reg_color);
+            page_5_text.Foreground = new SolidColorBrush(reg_color);
+
+        }
+
+        private void page_qu_4_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = quotation4;
+            home_text.Foreground = new SolidColorBrush(reg_color);
+            page_1_text.Foreground = new SolidColorBrush(reg_color);
+            page_2_text.Foreground = new SolidColorBrush(reg_color);
+            page_3_text.Foreground = new SolidColorBrush(reg_color);
+            page_4_text.Foreground = new SolidColorBrush(focus_color);
+            page_5_text.Foreground = new SolidColorBrush(reg_color);
+
+        }
+
+        private void page_qu_5_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = quotation5;
+            home_text.Foreground = new SolidColorBrush(reg_color);
+            page_1_text.Foreground = new SolidColorBrush(reg_color);
+            page_2_text.Foreground = new SolidColorBrush(reg_color);
+            page_3_text.Foreground = new SolidColorBrush(reg_color);
+            page_4_text.Foreground = new SolidColorBrush(reg_color);
+            page_5_text.Foreground = new SolidColorBrush(focus_color);
+
+        }
+
+        private void exit_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(1);
         }
     }
 }
